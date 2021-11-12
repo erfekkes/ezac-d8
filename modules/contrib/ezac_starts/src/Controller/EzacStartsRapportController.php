faed46e7df9cb44e46af1e9e7b3298670c092ec9
Evert Fekkes <evert@efekkes.nl>
2021-11-12 21:53:00 +0100

    EzacStartsRapportController.php aangepast voor berekening werkuren

--- modules/contrib/ezac_starts/src/Controller/EzacStartsRapportController.php
+++ modules/contrib/ezac_starts/src/Controller/EzacStartsRapportController.php
@@ -8,12 +8,19 @@ use Drupal\Core\Url;
 
 use Drupal\ezac_starts\Model\EzacStart;
 use Drupal\ezac\Util\EzacUtil;
+use Drupal\ezac_kisten\Model\EzacKist;
 
 /**
  * Controller for EZAC start administration.
  */
 class EzacStartsRapportController extends ControllerBase {
 
+  /**
+   * @param null $datum
+   * @param null $vlieger
+   *
+   * @return array|array[]
+   */
   public static function startRapport($datum = NULL, $vlieger = NULL) {
     $messenger = Drupal::messenger();
 
@@ -91,7 +98,6 @@ class EzacStartsRapportController extends ControllerBase {
 
         $row[] = array(
           t("<a href=$urlString>$naam</a>"),
-          //'<a href=' .BASE_PATH() .'startlijst/browse/' .$dat .'/' .$afkorting .'>' .$naam .'</a>',
           $aantal_dagen,
           $dag,
           $totaal,
@@ -197,41 +203,79 @@ class EzacStartsRapportController extends ControllerBase {
     $sortdir = 'ASC'; // deprecated
 
     $startsIndex = EzacStart::index($condition, $field, $sortkey, $sortdir);
-    foreach ($startsIndex as $id) {
-      $start = new EzacStart($id);
+    if ($startsIndex != []) {
+      foreach ($startsIndex as $id) {
+        $start = new EzacStart($id);
 
-      $datum = substr($start->datum,0,10);
-      $duur  = substr($start->duur,0,5); //strip seconds
-      $duur_hhmm = explode(':', $duur);
-      $duur_minuten = (int) $duur_hhmm[0] * 60 + (int) $duur_hhmm[1];
+        $datum = substr($start->datum, 0, 10);
+        $duur = substr($start->duur, 0, 5); //strip seconds
+        $duur_hhmm = explode(':', $duur);
+        $duur_minuten = (int) $duur_hhmm[0] * 60 + (int) $duur_hhmm[1];
 
-      /* Check of de vlucht voor eigen rekening is of club / prive */
-      if (isset($vlieger)) { // only when selected list is for a specific person
-        if (in_array($start->startmethode, array('S', 'M')) // sleep (tow) or motor (self launch)
-          or (($start->tweede == $vlieger) and ($start->instructie == FALSE))// not PIC
-          or (($start->gezagvoerder == $vlieger) and ($start->instructie == TRUE))// instructor
-          or (in_array($start->soort, array('CLUB', 'DONA', 'PASS'))) //CLUB, DONATEUR or PASSENGER flight
-        )
-        {
+        // prive kist?
+        $prive = true; // ook als kist niet in tabel voorkomt
+        $kist_id = EzacKist::getId($start->registratie);
+        if ($kist_id != null) {
+          $kist = new EzacKist($kist_id);
+          $prive = $kist->prive;
+        }
+
+        /* Check of de vlucht voor eigen rekening is of club / prive
+        [Mickel] Voor de berekening houden we wel rekening met:
+        de prive kisten, deze worden wel voor de lierstart belast maar niet voor de uren die ze er mee vliegen,
+        en bij slepen worden alleen de uren belast als het een club kist is,
+        */
+        if (isset($vlieger)) { // only when selected list is for a specific person
+          // eigen start?
+          if ((($start->gezagvoerder == $vlieger and $start->instructie == false) // geen instructeur
+            or ($start->tweede == $vlieger and $start->instructie == true) // instructiestart
+            or ($start->tweede == $vlieger and $start->soort == '2E')) // rekening tweede inzittende
+            and (!in_array($start->soort, ['CLUB','DONA','PASS']))) { // geen CLUB, DONA of PASS start
+            // eigen start
+            if ($start->startmethode == 'S') {
+              // bij slepen worden alleen de uren belast als het een club kist is,
+              if (!$prive) $eigen_minuten = $duur_minuten;
+              else $eigen_minuten = 0;
+              $eigen_starts = 0; // start telt niet mee
+            }
+            elseif ($start->startmethode == 'M') {
+              $eigen_minuten = 0; // duur telt niet mee
+              $eigen_starts = 0; // start telt niet mee
+            }
+            elseif ($start->startmethode == 'L') {
+              if (!$prive) $eigen_minuten = $duur_minuten; // duur telt alleen mee bij clubkisten
+              else $eigen_minuten = 0;
+              $eigen_starts = 1; // start telt wel mee
+            }
+            // B (bungee) buiten beschouwing gelaten
+          }
+          else {
+            // geen eigen start
+            $eigen_starts = 0;
+            $eigen_minuten = 0;
+          }
+        }
+        else {
+          // niet tellen per vlieger
+          $eigen_starts = 0;
           $eigen_minuten = 0;
         }
-        else $eigen_minuten = $duur_minuten;
-      }
 
-      if (!isset($index[$datum])) {
-        $index[$datum]['aantal'] = 1;
-        $index[$datum]['duur'] = $duur_minuten;
-        if ($eigen_minuten > 0) $index[$datum]['eigen_aantal'] = 1;
-        else $index[$datum]['eigen_aantal'] = 0;
-        $index[$datum]['eigen_duur'] = $eigen_minuten;
-      }
-      else {
-        $index[$datum]['aantal']++; // increment value for each record
-        $index[$datum]['duur'] += $duur_minuten;
-        if ($eigen_minuten > 0) $index[$datum]['eigen_aantal']++;
-        $index[$datum]['eigen_duur'] += $eigen_minuten;
-      }
-    } //foreach
+        // update counters
+        if (!isset($index[$datum])) {
+          $index[$datum]['aantal'] = 1;
+          $index[$datum]['duur'] = $duur_minuten;
+          $index[$datum]['eigen_aantal'] = $eigen_starts;
+          $index[$datum]['eigen_duur'] = $eigen_minuten;
+        }
+        else {
+          $index[$datum]['aantal']++; // increment value for each record
+          $index[$datum]['duur'] += $duur_minuten;
+          $index[$datum]['eigen_aantal'] += $eigen_starts;
+          $index[$datum]['eigen_duur'] += $eigen_minuten;
+        }
+      } //foreach
+    }
     if (isset($index)) return $index;
     else return [];
   }
